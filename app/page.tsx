6738ec8a957f30d2dfae7790b0030fcb42d8c363
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-08-01 17:33:36 -0300

    CarCard component created and implemented show all the information fecthed from the API. New props added to CustomButton component: textStyles,rigthIcon and isDisable.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
+
       <Hero />
 
       <div className="mt-12 padding-x padding-y max-width" id="discover">
@@ -14,11 +20,28 @@ export default function Home() {
 
         <div className="home__filters">
           <SearchBar />
+        
+          <div className="home__filter-container">
+            <CustomFilter title='fuel' />
+            <CustomFilter title='year' />
+          </div>
         </div>
-        <div className="home__filter-container">
-          <CustomFilter title='fuel' />
-          <CustomFilter title='year' />
-        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) =>(
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ): (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl"> No results found</h2>
+            <p>{allCars?.message}</p>
+          </div>  
+        )}
+
       </div>
     </main>
   );
